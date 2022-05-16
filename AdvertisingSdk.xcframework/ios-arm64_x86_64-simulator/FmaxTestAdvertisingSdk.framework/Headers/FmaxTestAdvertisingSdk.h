0c87a96c59924cc82454bfabf4d471454d8dbb60
Maxim Firsov <firsoffmaxim@gmail.com>
2022-05-16 11:41:18 +0500

    next

---
+++ AdvertisingSdk.xcframework/ios-arm64_x86_64-simulator/FmaxTestAdvertisingSdk.framework/Headers/FmaxTestAdvertisingSdk.h
@@ -0,0 +1,18 @@
+//
+//  FmaxTestAdvertisingSdk.h
+//  FmaxTestAdvertisingSdk
+//
+//  Created by Maksim on 16.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FmaxTestAdvertisingSdk.
+FOUNDATION_EXPORT double FmaxTestAdvertisingSdkVersionNumber;
+
+//! Project version string for FmaxTestAdvertisingSdk.
+FOUNDATION_EXPORT const unsigned char FmaxTestAdvertisingSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FmaxTestAdvertisingSdk/PublicHeader.h>
+
+
